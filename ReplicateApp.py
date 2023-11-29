2292d8f3e1ef5a6422b78903fb70f8e622090dd6
Pinde Fu <pfu@esri.com>
2023-11-29 11:46:37 -0800

    Clone and logging

--- ReplicateApp.py
+++ ReplicateApp.py
@@ -10,6 +10,8 @@ import time
 import json
 import os
 import re
+import sys
+import arcgis
 
 logger = None
 batch_size = 2500
@@ -156,19 +158,6 @@ def run_update(the_func):
 
     return wrapper
 
-def readAssignments(survey123Layer):
-
-    logger.info("\n ------ Query for current assignments ------ ")
-
-    fs = survey123Layer.query("1=1", out_fields="SiteID, assignedTo", order_by_fields="SiteID", return_distinct_values=True, return_all_records=True, return_geometry=False)
-    logger.info("\n Number of assignments returned  {}\n".format(len(fs.features)))
-
-    siteId_Evaluator_Lookup = {}
-    for f in fs.features:
-        siteId_Evaluator_Lookup[f.attributes["SiteID"]] = f.attributes["assignedTo"]
-
-    logger.info("\n\n siteId_Evaluator_Lookup {}".format(siteId_Evaluator_Lookup))
-    return siteId_Evaluator_Lookup
 
 def getFolder(folder_name):
     me = gis.users.me
@@ -177,24 +166,24 @@ def getFolder(folder_name):
 
     if folder_name not in folder_list:
         gis.content.create_folder(folder_name)
-        logger.info("\n\nFolder {} created".format(folder_name))
+        logger.info('\nFolder "{}" created'.format(folder_name))
     else:
-        logger.info("\n\nFolder {} already exists".format(folder_name))
+        logger.info('\nFolder "{}" already exists'.format(folder_name))
 
     return folder_name
 
 def queryMapExtent(ext_lyr_url, sWhere):
     # Query for the default extent for the web map
     extent_lyr = FeatureLayer(ext_lyr_url)
-    response = extent_lyr.query(where=sWhere, return_extent_only=True)
+    response = extent_lyr.query(where=sWhere, return_extent_only=True, out_sr=102100)
     logger.info("\n\nExtent query response: {}".format(response))
     country_extent = response["extent"]
     return country_extent
 
-def copyFLayerSchema(flyr_items, country, new_folder):
+def cloneFeatureLayers(fLyr_items, country, new_folder):
 
     itemId_lookup = {}
-    for itmId in flyr_items:
+    for itmId in fLyr_items:
         lyr_item = gis.content.get(itmId)
         lyr_url = lyr_item.url
         # Get the service name from the layer url
@@ -204,7 +193,7 @@ def copyFLayerSchema(flyr_items, country, new_folder):
         table_ids = [table.properties.id for table in lyr_item.tables]
 
         new_service_name = "{}_{}".format(service_name, country)
-        logger.info("\n\nTo create the layer : {}".format(new_service_name))
+        logger.info("\nCreating layer: {}".format(new_service_name))
         # copy the feature layers and name them with the country name
         item_copy = lyr_item.copy_feature_layer_collection(
             service_name = new_service_name,
@@ -212,9 +201,9 @@ def copyFLayerSchema(flyr_items, country, new_folder):
             tables = table_ids,
             folder = new_folder
         )
+        logger.info("\nLayer created. Id: {}".format(item_copy.id))
         itemId_lookup[itmId] = item_copy.id
 
-    logger.info("\n\nItem id lookup: {}".format(itemId_lookup))
     return itemId_lookup
 
 def getUrlLookup(itemId_lookup):
@@ -233,7 +222,7 @@ def getUrlLookup(itemId_lookup):
     return url_lookup
 
 
-def processTask(task, naming_patterns, init_extent_config, flyr_items, webmap_item, wab_template_item):
+def processTask(task, naming_patterns, template_country_name, init_extent_config, fLyr_items, imgLyr_items, webmap_item, wab_template_item):
     country = task["country"]
     new_folder_name = naming_patterns["folder_pattern"].replace('{country}', country)
     new_folder = getFolder(new_folder_name)
@@ -243,136 +232,146 @@ def processTask(task, naming_patterns, init_extent_config, flyr_items, webmap_it
     sWhere = init_extent_config["where"].replace('{country}', country)
     country_extent = queryMapExtent(ext_lyr_url, sWhere)
 
-    # itemId_lookup = copyFLayerSchema(flyr_items, country, new_folder)
-    itemId_lookup = {'3ae011ba86d14354b32300f59725ee97': 'dde6bb2474dd45179709a3cbfe32aee7', '453b9c3fba714775aa9e776e49d50f1f': '4e110bda1e6441a1bc70ecb74a8c24ef'}
+    itemId_lookup = cloneFeatureLayers(fLyr_items, country, new_folder)
+    if len(imgLyr_items) > 0 and "img_lyr_itemd_id" in task:
+        itemId_lookup[imgLyr_items[0]] = task['img_lyr_itemd_id']
 
-    url_lookup = getUrlLookup(itemId_lookup)
+    logger.info("\n\nLayer id lookup: {}".format(itemId_lookup))
 
+    new_webmap = cloneWebMap(webmap_item, new_folder, itemId_lookup, country_extent, country, naming_patterns)
+    new_webmap_id = new_webmap.item.id
+    itemId_lookup[webmap_item.id] = new_webmap_id
 
-    new_webmap = copyWebMap(webmap_item, new_folder, itemId_lookup, url_lookup, country_extent, country, naming_patterns)
-    return
-    new_wab = createWABApp(wab_template_item, new_webmap, itemId_lookup, url_lookup, country, naming_patterns)
+    logger.info("\n\nLayer and map id lookup: {}".format(itemId_lookup))
 
-def replaceUrlItemIds(aJson, itemId_lookup, url_lookup):
-    str_json = json.dumps(aJson)
-    # replace the layer urls, ignore the upper/lower case
-    for old_url in url_lookup:
-        new_url = url_lookup[old_url]
-        logger.info("\n\nReplacing url {} with {}".format(old_url, new_url))
-        str_json = replace_ignore_case(str_json, old_url, new_url)
+    new_wab = cloneWABApp(wab_template_item, new_webmap, itemId_lookup, country_extent, template_country_name, new_folder, country, naming_patterns)
+    return new_wab
 
-    # replace the layer item ids
-    for old_id in itemId_lookup:
-        new_id = itemId_lookup[old_id].id
-        logger.info("\n\nReplacing item id {} with {}".format(old_id, new_id))
-        str_json = replace_ignore_case(str_json, old_id, new_id)
+def migrate_resources(old_item, new_item, overwrite=True):
+    """
+    Migrate resources from one item to another.
+    """
+    from os.path import basename, dirname, join, exists
+    results = []
+    for resource in old_item.resources.list():
+        try:
+#             print(f"Migrating {resource['resource']}")
+            folder = dirname(resource['resource'])
+            file_name = basename(resource['resource'])
+            file = old_item.resources.get(resource['resource'])
+            if type(file) == str and exists(file):
+                res = new_item.resources.add(
+                    file = file,
+                    file_name = file_name,
+                    folder_name = folder
+                )
 
+            else:
+                res = new_item.resources.add(
+                    text = file,
+                    file_name = file_name,
+                    folder_name = folder
+                )
+            results.append(res)
+        except Exception as e:
+            print(e)
+            print('trying to update')
+            if overwrite:
+                print('overwriting')
+                if type(file) != str or not exists(file):
+                    text = file
+                    file = 'temp.txt'
+                    with open(file, 'w') as f:
+                        f.write(text)
+                res = new_item.resources.update(
+                    file = file,
+                    file_name = file_name,
+                    folder_name = folder
+                )
+                results.append(res)
+
+    return results
+
+# Replace the country name in the json using regular expression
+# The pattern is " sourceCountryName ". The space before and after the country name may vary. There may be 0 or more spaces
+# When replacing the country name, keep the space before and after the country name
+# When matching the country name, ignore the cases
+def replaceCountryName(str_json, sourceCountryName, targetCountryName):
+    pattern = r'(?i)(\s*)' + sourceCountryName + r'(\s*)'
+    str_json = re.sub(pattern, r'\1' + targetCountryName + r'\2', str_json)
     return str_json
 
-def createItemFromJson(oldItem, str_json, newTitle, country):
-    # try clone the item first, then update the title, tags, and text
 
-    new_item_properties = {} #oldItem.properties
-    # remove the id, owner, created, and url properties
-    # new_item_properties.pop("id", None)
-    # new_item_properties.pop("owner", None)
-    # new_item_properties.pop("created", None)
-    # new_item_properties.pop("url", None)
-    new_item_properties["title"] = newTitle
-    new_item_properties["tags"] = "{},{}".format(oldItem.tags, country)
-    new_item_properties["typeKeywords"] = oldItem.typeKeywords
-    new_item_properties["description"] = oldItem.description
-    new_item_properties["snippet"] = oldItem.snippet
-    new_item_properties["accessInformation"] = oldItem.accessInformation
-    new_item_properties["licenseInfo"] = oldItem.licenseInfo
-    new_item_properties["type"] = oldItem.type
-    new_item_properties["text"] = str_json
+def cloneWABApp(wab_template_item, new_webmap, itemId_lookup, country_extent, template_country_name, new_folder, country, naming_patterns):
+    logger.info("\nTo clone the app")
+    cloned_items = gis.content.clone_items(items=[wab_template_item], folder=new_folder, copy_data=False, search_existing_items=False, item_mapping = itemId_lookup)
+    new_wab_item = cloned_items[0]
+    logger.info("\nCloned the app. New ID: {}".format(new_wab_item.id))
 
-    logger.info("\n\nCreating the new item: {}".format(new_item_properties["title"]))
-    return gis.content.add(item_properties = new_item_properties)
-
-
-
-def createWABApp(wab_template_item, new_webmap, itemId_lookup, url_lookup, country, naming_patterns):
-    wab_json = wab_template_item.get_data(try_json=True)
-    str_json = replaceUrlItemIds(wab_json, itemId_lookup, url_lookup)
-
-    appTitle = naming_patterns["wab_pattern"].replace('{country}', country)
-
-    # In the web app json:
-    #   replace the web map item id
-    #   replace the layer urls
-    # name the new web app with the country name
-    wab_json = wab_template_item.get_data(try_json=True)
+    logger.info("\nTo update the app title, country text, init extent, and appItemId")
+    wab_json = new_wab_item.get_data(try_json=True)
+    appTitle = naming_patterns["app_title_patern"].replace('{country}', country)
     wab_json["title"] = appTitle
-    wab_json['map']['itemId'] = new_webmap.id
-    wab_json['map']['mapOptions']['extent'] = new_webmap.extent
+    wab_json["appItemId"] = new_wab_item.id
+    wab_json['map']['mapOptions']['extent'] = country_extent
 
-    return createItemFromJson(webmap_item, str_json, appTitle, country)
+    str_json = json.dumps(wab_json)
+    str_json = replaceCountryName(str_json, template_country_name, country)
+    new_wab_item.update(item_properties = {"title": appTitle}, data = str_json)
+    logger.info("\nApp updated successfully")
 
+    #migrate_resources(wab_template_item, new_wab_item)
 
-def copyWebMap(webmap_item, new_folder, itemId_lookup, url_lookup, country_extent, country, naming_patterns):
+    return new_wab_item
 
+def cloneWebMap(webmap_item, new_folder, itemId_lookup, country_extent, country, naming_patterns):
+    logger.info("\nTo clone the web map")
+    cloned_items = gis.content.clone_items(items=[webmap_item], folder=new_folder, copy_data=False, search_existing_items=False, item_mapping = itemId_lookup)
+    logger.info("\nCloned the web map. ID: {}".format(cloned_items[0].id))
+    logger.info("\nTo update the web map title")
+    newMap = WebMap(cloned_items[0])
     newTitle = naming_patterns["webmap_title_patern"].replace('{country}', country)
-    newMap_properties = {}
-    newMap_properties["title"] = newTitle
-    newMap_properties["snippet"] = webmap_item.snippet
-    newMap_properties["tags"] = "{},{}".format(webmap_item.tags, country)
-    newMap_properties["typeKeywords"] = webmap_item.typeKeywords
-
-    newWebMap = WebMap(webmap_item)
-    newWebMap.extent = country_extent
-    # loop through the layers and replace the layer urls and item ids
-    for layer in newWebMap.layers:
-        try:
-            layer['itemId'] = itemId_lookup[layer['itemId']]
-            layer['url'] = url_lookup[layer['url']]
-        except Exception as e:
-            pass
-
-    newMap_item = WebMap(webmap_item).save(newMap_properties, folder=new_folder)
-
-    return newMap_item
-
-    # Clone the web map
-    newMap_item = gis.content.clone_items(items=[webmap_item.id], folder=new_folder, copy_data=False, search_existing_items=False, item_mapping = itemId_lookup)
-
-    newMap = WebMap(newMap_item)
-    newMap.extent = country_extent
+    # set the new map's extent to the country extent
+    newMap.item.extent = country_extent
+    newMap.item.spatialReference = country_extent['spatialReference']
     newMap.update(item_properties = {"title": newTitle})
-
+    logger.info("\nWeb map updated successfully")
     return newMap
-
-    webmap_json = newMap.get_data(try_json=True)
-    str_json = replaceUrlItemIds(webmap_json, itemId_lookup, url_lookup)
-
-    return createItemFromJson(webmap_item, str_json, newTitle, country)
 
 def get_layer_item_ids(wm):
     wmo = WebMap(wm)
-    wm_id_list = []
+    fl_item_id_list = []
+    il_item_id_list = []
     for layer in wmo.layers:
+        # if layer url has ImageServer in it, regardless upper or lower case, it is an image layer
+        if "imageserver" in layer['url'].lower():
+            if not layer['itemId'] in il_item_id_list:
+                il_item_id_list.append(layer['itemId'])
+
         try:
             fsvc = FeatureLayerCollection(layer['url'][:-1], gis)
-            if not fsvc.properties['serviceItemId'] in wm_id_list:
-                wm_id_list.append(fsvc.properties['serviceItemId'])
+            if not fsvc.properties['serviceItemId'] in fl_item_id_list:
+                fl_item_id_list.append(fsvc.properties['serviceItemId'])
         except Exception as e:
             pass
-    return wm_id_list
+    return fl_item_id_list, il_item_id_list
 
 def getAppMapLayerItems(wab_template_itemId):
+    logger.info("\n*********** Inspect the template app {} *************".format(wab_template_itemId))
     # Get the web map json
     wab_template_item = gis.content.get(wab_template_itemId)
     wab_template_json = wab_template_item.get_data(try_json=True)
 
     # get the web map
     webmap_id = wab_template_json['map']['itemId']
-    logger.info("\n\nWeb map id: {}".format(webmap_id))
-    webmap_item = gis.content.get(webmap_id)
-    items_in_wm = get_layer_item_ids(webmap_item)
-    logger.info("\n\nItems in web map: {}".format(items_in_wm))
 
-    return wab_template_item, webmap_item, items_in_wm
+    logger.info("\n\tWeb map id: {}".format(webmap_id))
+    webmap_item = gis.content.get(webmap_id)
+    fl_items_in_wm, imgl_items_in_wm = get_layer_item_ids(webmap_item)
+    logger.info("\n\tFeature layer items in web map: {}".format(fl_items_in_wm))
+    logger.info("\n\tImage layer items in web map: {}".format(imgl_items_in_wm))
+
+    return wab_template_item, webmap_item, fl_items_in_wm, imgl_items_in_wm
 
 if __name__ == "__main__":
 
@@ -389,6 +388,8 @@ if __name__ == "__main__":
     # Get Logger & Log Directory
     log_folder = parameters["log_folder"]
     logger, log_dir, log_file_name = get_logger(log_folder, this_filename, start_time)
+    logger.info("Python version {}".format(sys.version))
+    logger.info("ArcGIS Python API version {}".format(arcgis.__version__))
 
     try:
         the_portal = parameters['the_portal']
@@ -398,21 +399,21 @@ if __name__ == "__main__":
         gis = GIS(portal_url, the_username, the_password)
 
         wab_template_itemId = parameters['wab_template_itemId']
-        wab_template_item, webmap_item, flyr_items = getAppMapLayerItems(wab_template_itemId)
-
+        wab_template_item, webmap_item, fLyr_items, imgLyr_items = getAppMapLayerItems(wab_template_itemId)
+        template_country_name = parameters['template_country']
         naming_patterns = parameters['naming_patterns']
         init_extent_config = parameters['init_extent_config']
 
         tasks = parameters["tasks"]
         for task in tasks:
-            logger.info("\n\n\n *********** {} *************".format(task["country"]))
+            logger.info("\n\n *********** Processing: {} *************".format(task["country"]))
             task_start_time = time.time()
-            processTask(task, naming_patterns, init_extent_config, flyr_items, webmap_item, wab_template_item)
-            logger.info("\n\n\n ... task run time: {0} Minutes".format(round(((time.time() - task_start_time) / 60), 2)))
+            processTask(task, naming_patterns, template_country_name, init_extent_config, fLyr_items, imgLyr_items, webmap_item, wab_template_item)
+            logger.info("\n ------ task run time: {0} Minutes".format(round(((time.time() - task_start_time) / 60), 2)))
 
     except Exception:
         logger.info("\n\n{}".format(traceback.format_exc()))
 
     finally:
         # Log Run Time
-        logger.info('Program Run Time: {0} Minutes'.format(round(((time.time() - start_time) / 60), 2)))
+        logger.info('\n\nProgram Run Time: {0} Minutes\n\n'.format(round(((time.time() - start_time) / 60), 2)))
